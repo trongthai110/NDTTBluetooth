6b223d0d1fe9d5fc171c8768a85aab1f2c7799e2
Nguyen Dang Trong Thai <nguyendangtrongthai25@gmail.com>
2024-01-23 20:25:28 +0700

    Update Version

---
+++ Example/NDTTBluetooth/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  NDTTBluetooth
+//
+//  Created by Nguyen Dang Trong Thai on 01/23/2024.
+//  Copyright (c) 2024 Nguyen Dang Trong Thai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
